6f31d4dcaec3667048b211904274071e55d3fb1e
404Monkey <cbenjamin174@gmail.com>
2020-11-29 11:40:02 +0100

    beginning of generate game()

--- src/Game.java
+++ src/Game.java
@@ -8,9 +8,59 @@ public class Game {
 
 	// ***** CONSTRUCTORS *****
 
-	public void GenerateGame()
+	public Game(String heroName)
 	{
-	
+		// We create the rooms
+		Place animalRoom = new Place("Animal room", false, true);
+		Place transferRoom = new Place("Transfer room", false, true);
+		Place changingRoom = new Place("Changing room", false, true);
+		Place entry = new Place("Entry", false, true);
+		Place meetingRoom = new Place("Meeting room", false, true);
+		Place archivesRoom = new Place("Archives room", false, true);
+		Place experimentsRoom = new Place("Experiments room", false, true);
+		Place mortuary = new Place("Mortuary", false, true); // on éclaire la pièce ?
+		Place condamnedSAS = new Place("Condamned sas", true, true);
+		Place desertedRoom = new Place("Deserted room", false, true);
+		Place productsReserve = new Place("Products reserve", false, true);
+		Place garbageRoom = new Place("Garbage room", false, true);
+		Place coldRoom = new Place("Cold room", false, false);
+		Place dirtyChangingRoom = new Place("Dirty changing room", false, true); // est ce qu'on pourrait pas l'enlever ?
+		Place decontaminationRoom = new Place("Decontamination room", false, true);
+		Place exit = new Place("Exit", false, true);
+
+		// We create the doors
+		Door changAndEntry = new CondemnedDoor(changingRoom, entry);
+
+		Door meetAndArch = new DestructableDoor(meetingRoom, archivesRoom);
+
+		Door transfAndMeet = new LockedKeyDoor(1, transferRoom, meetingRoom);
+		Door decontAndExit = new LockedKeyDoor(2, decontaminationRoom, exit);
+
+		Door animAndTransf = new SecretCodeDoor("MOT DE PASSE DU CHAT", animalRoom, transferRoom);
+		Door experimAndReserv = new SecretCodeDoor("MOT DE PASSE DE LA SOURIS", experimentsRoom, productsReserve);
+		Door experimAndDirty = new SecretCodeDoor("MOT DE PASSE DU SINGE", experimentsRoom, dirtyChangingRoom);
+
+		Door transfAndChang = new Door(transferRoom, changingRoom);
+		Door transfAndExper = new Door(transferRoom, experimentsRoom);
+		Door experAndMort = new Door(experimentsRoom, mortuary);
+		Door experAndConda = new Door(experimentsRoom, condamnedSAS);
+		Door condaAndDesert = new Door(condamnedSAS, desertedRoom);
+		Door experAndCold = new Door(experimentsRoom, coldRoom);
+		Door coldAndGarb = new Door(coldRoom, garbageRoom);
+		Door dirtAndDecon = new Door(dirtyChangingRoom, decontaminationRoom);
+
+
+		// We add the doors to the rooms
+
+
+		// We add the animals to the rooms
+
+		// We add the objects to the rooms
+
+		// We add the enemies to the rooms
+
+		// We create the Hero
+		this.hero = new Hero(heroName, animalRoom);
 	}
 
 	// ***** METHODS *****
