eadef016a9edf54003697f112926481a3678a694
GaetanFrejoux <gaetanfrejoux@gmail.com>
2020-12-02 08:45:38 +0100

    ajout classe FireStick.java

---
+++ src/FiredStick.java
@@ -0,0 +1,29 @@
+public class FiredStick extends Obj {
+	
+	
+	
+	
+	
+	// ***** CONSTANTS *****
+	
+	// ***** ATTRIBUTES *****
+	
+	// ***** CONSTRUCTORS *****
+	public FiredStick(String name) {
+		super(name);
+	}
+	// ***** METHODS *****
+	@Override
+	public void use(Hero h) {
+		//TODO
+	}
+	// Getter
+	
+	
+	
+	// Setter
+	
+	
+	
+	// Display
+}
