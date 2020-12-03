969db915f6708877479d0ac119c72f4699dafcdf
404Monkey <cbenjamin174@gmail.com>
2020-12-03 21:26:07 +0100

    KeyTest.java + coorection remove key du loot de l'ennemy dans battle

---
+++ Test/objectsTest/KeyTest.java
@@ -0,0 +1,49 @@
+package objectsTest;
+
+import characters.Hero;
+import objects.Key;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import others.Place;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KeyTest {
+
+    private Hero h;
+    private Place a;
+    private Key k;
+
+    @BeforeEach
+    void setUp() {
+        a = new Place("Place", false, true);
+
+        k = new Key("Key");
+        a.addObject(k);
+
+        h = new Hero("Test", a);
+    }
+
+    @Test
+    void use() {
+        k.use(h);
+    }
+
+    @Test
+    void testUse() {
+        k.use(h);
+    }
+
+    @Test
+    void take() {
+        h.take("Key");
+        assertTrue(h.getObjs().containsValue(k));
+        assertFalse(h.getPlace().getItems().containsValue(k));
+        assertEquals(Hero.DEFAULT_KEY_LEVEL+1, h.getKeyLevel());
+    }
+
+    @Test
+    void look() {
+        k.look();
+    }
+}
